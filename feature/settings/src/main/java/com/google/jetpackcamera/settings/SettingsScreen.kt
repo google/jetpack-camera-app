7ec2b4730d1e44e3b8bb94552245110ec2e9f002
Kimberly Crevecoeur <kcrevecoeur@google.com>
2023-04-20 12:46:42 +0000

    Messing around with initial UI Components

--- feature/settings/src/main/java/com/google/jetpackcamera/settings/SettingsScreen.kt
+++ feature/settings/src/main/java/com/google/jetpackcamera/settings/SettingsScreen.kt
@@ -16,11 +16,22 @@
 
 package com.google.jetpackcamera.settings
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowRight
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ListItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.compose.material3.*
+import androidx.compose.ui.graphics.vector.ImageVector
 
 /**
  * Screen used for the Settings feature.
@@ -30,6 +41,52 @@ fun SettingsScreen(
     viewModel: SettingsViewModel = hiltViewModel()
 ) {
     val settingsUiState by viewModel.settingsUiState.collectAsState()
+    Column {
+        pageHeader(settingsUiState.text)
+        sectionHeader(title = "general")
+        basicSettingUI(title = "Some setting", description = "some cool description")
+        basicSettingUI(title = "Some setting2", description = "some cool description")
+        basicSettingUI(title = "Some setting3", description = "some cool description")
+        basicSettingUI(title = "Some setting4", description = "some cool description")
+    }
+}
 
-    Text(settingsUiState.text)
+@Composable
+fun pageHeader(title:String) {
+    Text(
+        text = title,
+        modifier = Modifier
+    )
+    Divider()
+}
+
+@Composable
+fun sectionHeader(title: String) {
+    Text(
+        text = title,
+        modifier = Modifier
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun basicSettingUI(title: String, description:String) {
+    Box(modifier = Modifier) {
+        Column {
+            ListItem(
+                headlineText = {Text(title)} ,
+                  supportingText = {Text(description)},
+                  leadingContent = {
+                      Icon(Icons.Filled.Settings, contentDescription = "something" )
+                  },
+                trailingContent = {
+                    Icon(
+                        Icons.Filled.KeyboardArrowRight,
+                        contentDescription = "something"
+                    )
+                }
+        )
+        }
+        Divider()
+    }
 }
