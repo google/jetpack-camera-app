a031ab865b14e9ac0b30abb472d218bd9408acae
Yasith Vidanaarachchi <yasith@google.com>
2023-09-01 18:57:35 -0400

    Migrate build configuration from Groovy to Kotlin (#35)
    * Convert project build.gradle file to use Kotlin DSL
    
    - Also remove Compose Version const, instead use the version from the BOM
    
    (We'll move to using Version catalogs in a different PR)
    
    * Convert app/build.gradle to KTS
    
    * Convert camera-viewfinder-compose build file to KTS
    
    * Convert core/common/build.gradle to KTS
    
    * Convert data/settings/build.gradle to KTS
    
    * Convert domain/camera/build.gradle to KTS
    
    * Convert feature/preview/build.gradle to KTS
    
    * Convert feature/quicksettings/build.gradle to KTS
    
    * Convert feature/settings/build.gradle to KTS
    
    * Update build.gradle.kts
    
    * Convert settings.gradle to KTS

---
+++ camera-viewfinder-compose/build.gradle.kts
@@ -0,0 +1,71 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.google.jetpackcamera.camerax"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 21
+        targetSdk = 34
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlin {
+        jvmToolchain(17)
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.4.0"
+    }
+}
+
+dependencies {
+    // Compose
+    val composeBom = platform("androidx.compose:compose-bom:2023.08.00")
+    implementation(composeBom)
+    androidTestImplementation(composeBom)
+
+    // Compose - Material Design 3
+    implementation("androidx.compose.material3:material3")
+
+    // Compose - Testing
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+
+    // Compose - Android Studio Preview support
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.ui:ui-tooling")
+
+    // Testing
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.3")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+
+    // CameraX
+    val camerax_version = "1.3.0-SNAPSHOT"
+    implementation("androidx.camera:camera-core:${camerax_version}")
+    implementation("androidx.camera:camera-view:${camerax_version}")
+
+    // AndroidX Core
+    val core_version = "1.9.0"
+    implementation("androidx.core:core:{$core_version}")
+}
