fe046a4e2e0039f3ba0ce7ec23dc27189217cad8
Kimberly Crevecoeur <kcrevecoeur@google.com>
2023-10-12 17:26:46 +0000

    add loading spin animation to mainActivity

--- app/src/main/java/com/google/jetpackcamera/MainActivity.kt
+++ app/src/main/java/com/google/jetpackcamera/MainActivity.kt
@@ -20,15 +20,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
@@ -66,6 +76,16 @@ class MainActivity : ComponentActivity() {
         setContent {
             when (uiState) {
                 Loading -> {/* TODO(b/283812742):  a loading screen */
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(Color.Black),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        CircularProgressIndicator(modifier = Modifier.size(50.dp))
+                        Text(text = stringResource(R.string.jca_loading), color = Color.White)
+                    }
                 }
 
                 is Success -> {
