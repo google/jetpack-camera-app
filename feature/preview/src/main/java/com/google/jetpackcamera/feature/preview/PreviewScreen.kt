c39a58d09451cd8e8180ffc5385414ea4f622151
Kimberly Crevecoeur <kcrevecoeur@google.com>
2023-10-12 17:15:02 +0000

    Add a loading animation while CameraState is not ready

--- feature/preview/src/main/java/com/google/jetpackcamera/feature/preview/PreviewScreen.kt
+++ feature/preview/src/main/java/com/google/jetpackcamera/feature/preview/PreviewScreen.kt
@@ -20,6 +20,8 @@ import android.os.Handler
 import android.os.Looper
 import android.util.Log
 import androidx.camera.core.Preview.SurfaceProvider
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
@@ -30,6 +32,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.SuggestionChip
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -42,6 +47,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -96,7 +102,16 @@ fun PreviewScreen(
         }
     }
     if (previewUiState.cameraState == CameraState.NOT_READY) {
-        Text(text = stringResource(R.string.camera_not_ready))
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.Black),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            CircularProgressIndicator(modifier = Modifier.size(50.dp))
+            Text(text = stringResource(R.string.camera_not_ready), color = Color.White)
+        }
     } else if (previewUiState.cameraState == CameraState.READY) {
         // display camera feed. this stays behind everything else
         PreviewDisplay(
