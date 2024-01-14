c42a30829053eba062f74e5cf44e266ffafd2fe5
Trevor McGuire <trevormcguire@google.com>
2024-01-13 16:14:28 -0800

    Migrate to using AndroidX graphics for effects processor
     This effects processor uses the graphics-core library's GLRenderer
     to perform rendering rather than CameraX's internal OpenGLRenderer.
    
     This renderer can handle SDR and 10-bit HLG dynamic ranges.

--- domain/camera/build.gradle.kts
+++ domain/camera/build.gradle.kts
@@ -76,6 +76,9 @@ dependencies {
     // Tracing
     implementation("androidx.tracing:tracing-ktx:1.2.0")
 
+    // Graphics libraries
+    implementation("androidx.graphics:graphics-core:1.0.0-beta01")
+
     // Project dependencies
     implementation(project(":data:settings"))
     implementation(project(":core:common"))
