806f02730dcdeaf8ff31015a18d16839514802d0
Kimberly Crevecoeur <kcrevecoeur@google.com>
2024-02-15 23:13:11 +0000

    spotless

--- data/settings/src/main/java/com/google/jetpackcamera/settings/SettingsRepository.kt
+++ data/settings/src/main/java/com/google/jetpackcamera/settings/SettingsRepository.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2023 The Android Open Source Project
+ * Copyright (C) 2023-2024 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -54,7 +54,10 @@ interface SettingsRepository {
 
     suspend fun updateTargetFrameRate(targetFrameRate: TargetFrameRate)
 
-    suspend fun updateSupportedFixedFrameRate(supportedFrameRates: Set<Int>, currentTargetFrameRate: TargetFrameRate)
+    suspend fun updateSupportedFixedFrameRate(
+        supportedFrameRates: Set<Int>,
+        currentTargetFrameRate: TargetFrameRate
+    )
 
     suspend fun getCameraAppSettings(): CameraAppSettings
 }
