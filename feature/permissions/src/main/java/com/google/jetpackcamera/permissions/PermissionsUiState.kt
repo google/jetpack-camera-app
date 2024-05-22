7075157c30d1717516e88bebca151fce8c77e06a
Kimberly Crevecoeur <kcrevecoeur@google.com>
2024-05-22 08:08:28 -0700

    Updated Permissions Request Flow (#193)
    updated permissions flow for JCA to be more scalable
    
    * migrate permissions files from app to permissions module
    
    * use routes to navigate to/from permissions screen instead of if/else check
    
    * assisted dependency injection for permissions screen viewmodel

--- feature/permissions/src/main/java/com/google/jetpackcamera/permissions/PermissionsUiState.kt
+++ feature/permissions/src/main/java/com/google/jetpackcamera/permissions/PermissionsUiState.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2023 The Android Open Source Project
+ * Copyright (C) 2024 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,10 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.google.jetpackcamera.ui
+package com.google.jetpackcamera.permissions
 
-object Routes {
-    const val PREVIEW_ROUTE = "preview"
-    const val SETTINGS_ROUTE = "settings"
-    const val PERMISSIONS_ROUTE = "permissions"
+sealed interface PermissionsUiState {
+    data class PermissionsNeeded(val currentPermission: PermissionEnum) : PermissionsUiState
+    object AllPermissionsGranted : PermissionsUiState
 }
