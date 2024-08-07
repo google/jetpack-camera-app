047b44a59c9b8b9fd52482a38075584889ca6c19
Trevor McGuire <trevormcguire@google.com>
2024-08-06 23:42:32 -0700

    Update build ID for CameraX snapshots
     This build ID includes change aosp/3180265 which fixes HDR
     shaders on devices using ANGLE driver.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -26,7 +26,7 @@ dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         maven {
-            setUrl("https://androidx.dev/snapshots/builds/11932573/artifacts/repository")
+            setUrl("https://androidx.dev/snapshots/builds/12143524/artifacts/repository")
         }
         google()
         mavenCentral()
