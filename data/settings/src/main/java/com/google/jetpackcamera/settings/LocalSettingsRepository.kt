f31c3fdc5c417a64e353e4882ae4a3d3e7372d2e
Yasith Vidanaarachchi <yasith@google.com>
2024-10-11 12:26:22 -0400

    Update flash_mode.proto

--- data/settings/src/main/java/com/google/jetpackcamera/settings/LocalSettingsRepository.kt
+++ data/settings/src/main/java/com/google/jetpackcamera/settings/LocalSettingsRepository.kt
@@ -103,7 +103,7 @@ class LocalSettingsRepository @Inject constructor(
             FlashMode.AUTO -> FlashModeProto.FLASH_MODE_AUTO
             FlashMode.ON -> FlashModeProto.FLASH_MODE_ON
             FlashMode.OFF -> FlashModeProto.FLASH_MODE_OFF
-            FlashMode.LOW_LIGHT_BOOST -> FlashModeProto.FLASH_MODE_OFF
+            FlashMode.LOW_LIGHT_BOOST -> FlashModeProto.FLASH_MODE_LOW_LIGHT_BOOST
         }
         jcaSettings.updateData { currentSettings ->
             currentSettings.toBuilder()
