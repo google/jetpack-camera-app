f13c104ee164595fabb8ac0279e9aff056e35773
Kimberly Crevecoeur <kcrevecoeur@google.com>
2024-10-17 15:55:09 -0700

    VideoRecordingState refactor (#271)
    * Refactor VideoRecordingState to update directly from the camera instead of UI
    * Move VideoRecordingState to CameraState
    * VideoRecordingState.Inactive stores the duration of the most recent recording
    * Convert VideoRecordingState.Active to interface, and implemented as Paused and Recording
    * Audio amplitude, elapsed time, and max video duration are stored in VideoRecordingState.Active

--- feature/preview/src/main/java/com/google/jetpackcamera/feature/preview/quicksettings/QuickSettingsScreen.kt
+++ feature/preview/src/main/java/com/google/jetpackcamera/feature/preview/quicksettings/QuickSettingsScreen.kt
@@ -38,6 +38,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.google.jetpackcamera.core.camera.VideoRecordingState
 import com.google.jetpackcamera.feature.preview.CaptureModeToggleUiState
 import com.google.jetpackcamera.feature.preview.PreviewMode
 import com.google.jetpackcamera.feature.preview.PreviewUiState
@@ -300,6 +301,7 @@ fun ExpandedQuickSettingsUiPreview() {
                 currentCameraSettings = CameraAppSettings(),
                 systemConstraints = TYPICAL_SYSTEM_CONSTRAINTS,
                 previewMode = PreviewMode.StandardMode {},
+                videoRecordingState = VideoRecordingState.Inactive(),
                 captureModeToggleUiState = CaptureModeToggleUiState.Invisible
             ),
             currentCameraSettings = CameraAppSettings(),
@@ -326,7 +328,8 @@ fun ExpandedQuickSettingsUiPreview_WithHdr() {
                 currentCameraSettings = CameraAppSettings(),
                 systemConstraints = TYPICAL_SYSTEM_CONSTRAINTS,
                 previewMode = PreviewMode.StandardMode {},
-                captureModeToggleUiState = CaptureModeToggleUiState.Invisible
+                captureModeToggleUiState = CaptureModeToggleUiState.Invisible,
+                videoRecordingState = VideoRecordingState.Inactive()
             ),
             currentCameraSettings = CameraAppSettings(dynamicRange = DynamicRange.HLG10),
             onLensFaceClick = { },
