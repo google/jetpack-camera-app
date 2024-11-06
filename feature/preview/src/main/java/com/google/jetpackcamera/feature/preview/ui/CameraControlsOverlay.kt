beb030beba6e652a74348cc8a0f4d4855b37e0f7
davidjiagoogle <125502967+davidjiagoogle@users.noreply.github.com>
2024-11-06 13:59:09 -0800

    New external intent capture mode for multiple image captures (#275)
    * multiple image captures
    
    * update
    
    * update
    
    * update
    
    * update
    
    * Update CameraControlsOverlay.kt
    
    * Update MainActivity.kt
    
    * address comments
    
    * address comments
    
    * Update MainActivity.kt
    
    * Update PreviewViewModel.kt
    
    * address comments
    
    * Update CameraControlsOverlay.kt
    
    * Update PreviewViewModel.kt
    
    * Update PreviewViewModel.kt

--- feature/preview/src/main/java/com/google/jetpackcamera/feature/preview/ui/CameraControlsOverlay.kt
+++ feature/preview/src/main/java/com/google/jetpackcamera/feature/preview/ui/CameraControlsOverlay.kt
@@ -100,7 +100,7 @@ fun CameraControlsOverlay(
         ContentResolver,
         Uri?,
         Boolean,
-        (PreviewViewModel.ImageCaptureEvent) -> Unit
+        (PreviewViewModel.ImageCaptureEvent, Int) -> Unit
     ) -> Unit = { _, _, _, _ -> },
     onStartVideoRecording: (
         Uri?,
@@ -241,7 +241,7 @@ private fun ControlsBottom(
         ContentResolver,
         Uri?,
         Boolean,
-        (PreviewViewModel.ImageCaptureEvent) -> Unit
+        (PreviewViewModel.ImageCaptureEvent, Int) -> Unit
     ) -> Unit = { _, _, _, _ -> },
     onToggleQuickSettings: () -> Unit = {},
     onToggleAudioMuted: () -> Unit = {},
@@ -343,7 +343,7 @@ private fun CaptureButton(
         ContentResolver,
         Uri?,
         Boolean,
-        (PreviewViewModel.ImageCaptureEvent) -> Unit
+        (PreviewViewModel.ImageCaptureEvent, Int) -> Unit
     ) -> Unit = { _, _, _, _ -> },
     onToggleQuickSettings: () -> Unit = {},
     onStartVideoRecording: (
@@ -365,16 +365,29 @@ private fun CaptureButton(
                         onCaptureImageWithUri(
                             context.contentResolver,
                             null,
-                            true,
-                            previewUiState.previewMode.onImageCapture
-                        )
+                            true
+                        ) { event: PreviewViewModel.ImageCaptureEvent, _: Int ->
+                            previewUiState.previewMode.onImageCapture(event)
+                        }
                     }
 
                     is PreviewMode.ExternalImageCaptureMode -> {
                         onCaptureImageWithUri(
                             context.contentResolver,
                             previewUiState.previewMode.imageCaptureUri,
-                            false,
+                            false
+                        ) { event: PreviewViewModel.ImageCaptureEvent, _: Int ->
+                            previewUiState.previewMode.onImageCapture(event)
+                        }
+                    }
+
+                    is PreviewMode.ExternalMultipleImageCaptureMode -> {
+                        val ignoreUri = previewUiState.previewMode.imageCaptureUris.isNullOrEmpty()
+                        onCaptureImageWithUri(
+                            context.contentResolver,
+                            null,
+                            previewUiState.previewMode.imageCaptureUris.isNullOrEmpty() ||
+                                ignoreUri,
                             previewUiState.previewMode.onImageCapture
                         )
                     }
@@ -384,7 +397,7 @@ private fun CaptureButton(
                             context.contentResolver,
                             null,
                             false
-                        ) {}
+                        ) { _: PreviewViewModel.ImageCaptureEvent, _: Int -> }
                     }
                 }
             }
