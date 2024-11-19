7ab1afc735031d84355595fbeb66aa339467db95
David Jia <davidjia@google.com>
2024-11-19 12:36:13 -0800

    Update DebugOverlayComponents.kt

--- feature/preview/src/main/java/com/google/jetpackcamera/feature/preview/ui/debug/DebugOverlayComponents.kt
+++ feature/preview/src/main/java/com/google/jetpackcamera/feature/preview/ui/debug/DebugOverlayComponents.kt
@@ -29,7 +29,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
