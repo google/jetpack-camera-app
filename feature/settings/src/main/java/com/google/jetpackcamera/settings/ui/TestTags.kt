f6c5ec2d27a6ca365afdd056fdd789f6d5b7bf8b
David Jia <davidjia@google.com>
2025-02-10 15:05:19 -0800

    Update TestTags.kt

--- feature/settings/src/main/java/com/google/jetpackcamera/settings/ui/TestTags.kt
+++ feature/settings/src/main/java/com/google/jetpackcamera/settings/ui/TestTags.kt
@@ -35,27 +35,32 @@ const val FPS_UNSUPPORTED_TAG = "FpsUnsupportedTag"
 const val DEFAULT_CAMERA_FACING_SWITCH_TAG = "DefaultCameraFacingSwitchTag"
 const val ENABLE_AUDIO_SWITCH_TAG = "EnableAudioSwitchTag"
 const val APP_VERSION_TAG = "AppVersionTag"
+
 // Flash Mode
 const val FLASH_MODE_SELECTOR_TAG = "FlashModeSelectorTag"
 const val FLASH_MODE_OPTION_AUTO_TAG = "FlashModeOptionAutoTag"
 const val FLASH_MODE_OPTION_ON_TAG = "FlashModeOptionOnTag"
 const val FLASH_MODE_OPTION_OFF_TAG = "FlashModeOptionOffTag"
 const val FLASH_MODE_OPTION_LOW_LIGHT_BOOST_TAG = "FlashModeOptionLowLightBoostTag"
+
 // Frame Rate
 const val FRAME_RATE_SELECTOR_TAG = "FrameRateSelectorTag"
 const val FRAME_RATE_OPTION_AUTO_TAG = "FrameRateOptionAutoTag"
 const val FRAME_RATE_OPTION_15_TAG = "FrameRateOption15Tag"
 const val FRAME_RATE_OPTION_30_TAG = "FrameRateOption30Tag"
 const val FRAME_RATE_OPTION_60_TAG = "FrameRateOption60Tag"
+
 // Aspect Ratio
 const val ASPECT_RATIO_SELECTOR_TAG = "AspectRatioSelectorTag"
 const val ASPECT_RATIO_OPTION_9_16_TAG = "AspectRatioOption9_16Tag"
 const val ASPECT_RATIO_OPTION_3_4_TAG = "AspectRatioOption3_4Tag"
 const val ASPECT_RATIO_OPTION_1_1_TAG = "AspectRatioOption1_1Tag"
+
 // Stream Configuration
 const val STREAM_CONFIG_SELECTOR_TAG = "StreamConfigSelectorTag"
 const val STREAM_CONFIG_OPTION_SINGLE_STREAM_TAG = "StreamConfigOptionSingleStreamTag"
 const val STREAM_CONFIG_OPTION_MULTI_STREAM_CAPTURE_TAG = "StreamConfigOptionMultiStreamCaptureTag"
+
 // Max Video Duration
 const val MAX_VIDEO_DURATION_SELECTOR_TAG = "MaxVideoDurationSelectorTag"
 const val MAX_VIDEO_DURATION_OPTION_UNLIMITED_TAG = "MaxVideoDurationOptionUnlimitedTag"
@@ -63,6 +68,7 @@ const val MAX_VIDEO_DURATION_OPTION_ONE_SEC_TAG = "MaxVideoDurationOptionOneSecT
 const val MAX_VIDEO_DURATION_OPTION_TEN_SEC_TAG = "MaxVideoDurationOptionTenSecTag"
 const val MAX_VIDEO_DURATION_OPTION_THIRTY_SEC_TAG = "MaxVideoDurationOptionThirtySecTag"
 const val MAX_VIDEO_DURATION_OPTION_SIXTY_SEC_TAG = "MaxVideoDurationOptionSixtySecTag"
+
 // Video Stabilization
 const val VIDEO_STABILIZATION_SELECTOR_TAG = "VideoStabilizationSelectorTag"
 const val VIDEO_STABILIZATION_OPTION_AUTO_TAG = "VideoStabilizationOptionAutoTag"
@@ -70,6 +76,7 @@ const val VIDEO_STABILIZATION_OPTION_ON_TAG = "VideoStabilizationOptionOnTag"
 const val VIDEO_STABILIZATION_OPTION_OFF_TAG = "VideoStabilizationOptionOffTag"
 const val VIDEO_STABILIZATION_OPTION_HIGH_QUALITY_TAG = "VideoStabilizationOptionHighQualityTag"
 const val VIDEO_STABILIZATION_OPTION_OPTICAL_TAG = "VideoStabilizationOptionOpticalTag"
+
 // Video Quality
 const val VIDEO_QUALITY_UNSUPPORTED_TAG = "VideoQualityUnsupportedTag"
 const val VIDEO_QUALITY_SELECTOR_TAG = "VideoQualitySelectorTag"
@@ -78,6 +85,7 @@ const val VIDEO_QUALITY_OPTION_SD_TAG = "VideoQualitySdTag"
 const val VIDEO_QUALITY_OPTION_HD_TAG = "VideoQualityHdTag"
 const val VIDEO_QUALITY_OPTION_FHD_TAG = "VideoQualityFhdTag"
 const val VIDEO_QUALITY_OPTION_UHD_TAG = "VideoQualityUhdTag"
+
 // Dark Mode
 const val DARK_MODE_SELECTOR_TAG = "DarkModeSelectorTag"
 const val DARK_MODE_OPTION_ON_TAG = "DarkModeOptionOnTag"
