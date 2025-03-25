ae7378cf337d51c9b36e59cd18e7deed1b14a3d5
Kimberly Crevecoeur <kcrevecoeur@google.com>
2025-03-25 12:56:34 +0000

    update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,6 +19,7 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.kapt)
     alias(libs.plugins.dagger.hilt.android)
+    alias(libs.plugins.compose.compiler)
 }
 
 android {
--- feature/permissions/build.gradle.kts
+++ feature/permissions/build.gradle.kts
@@ -16,6 +16,7 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.compose.compiler)
 
     alias(libs.plugins.kotlin.kapt)
     alias(libs.plugins.dagger.hilt.android)
