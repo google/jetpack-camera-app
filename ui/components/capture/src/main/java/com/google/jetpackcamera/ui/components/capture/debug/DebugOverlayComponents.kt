b1c72c244a9bbf1864610d1e04fab894577bfd67
Trevor McGuire <trevormcguire@google.com>
2025-09-06 23:04:37 -0700

    Add test tags to allow for setting test pattern in tests

--- ui/components/capture/src/main/java/com/google/jetpackcamera/ui/components/capture/debug/DebugOverlayComponents.kt
+++ ui/components/capture/src/main/java/com/google/jetpackcamera/ui/components/capture/debug/DebugOverlayComponents.kt
@@ -54,11 +54,25 @@ import androidx.compose.ui.unit.sp
 import com.google.jetpackcamera.model.TestPattern
 import com.google.jetpackcamera.ui.components.capture.DEBUG_OVERLAY_BUTTON
 import com.google.jetpackcamera.ui.components.capture.DEBUG_OVERLAY_CAMERA_PROPERTIES_TAG
+import com.google.jetpackcamera.ui.components.capture.DEBUG_OVERLAY_SET_TEST_PATTERN_BUTTON
 import com.google.jetpackcamera.ui.components.capture.DEBUG_OVERLAY_SET_ZOOM_RATIO_BUTTON
 import com.google.jetpackcamera.ui.components.capture.DEBUG_OVERLAY_SET_ZOOM_RATIO_SET_BUTTON
 import com.google.jetpackcamera.ui.components.capture.DEBUG_OVERLAY_SET_ZOOM_RATIO_TEXT_FIELD
 import com.google.jetpackcamera.ui.components.capture.DEBUG_OVERLAY_SHOW_CAMERA_PROPERTIES_BUTTON
 import com.google.jetpackcamera.ui.components.capture.DEBUG_OVERLAY_VIDEO_RESOLUTION_TAG
+import com.google.jetpackcamera.ui.components.capture.TEST_PATTERN_COLOR_BARS_BUTTON
+import com.google.jetpackcamera.ui.components.capture.TEST_PATTERN_COLOR_BARS_FADE_TO_GRAY_BUTTON
+import com.google.jetpackcamera.ui.components.capture.TEST_PATTERN_CUSTOM1_BUTTON
+import com.google.jetpackcamera.ui.components.capture.TEST_PATTERN_DIALOG_CLOSE_BUTTON
+import com.google.jetpackcamera.ui.components.capture.TEST_PATTERN_OFF_BUTTON
+import com.google.jetpackcamera.ui.components.capture.TEST_PATTERN_PN9_BUTTON
+import com.google.jetpackcamera.ui.components.capture.TEST_PATTERN_SOLID_COLOR_BLACK_BUTTON
+import com.google.jetpackcamera.ui.components.capture.TEST_PATTERN_SOLID_COLOR_BLUE_BUTTON
+import com.google.jetpackcamera.ui.components.capture.TEST_PATTERN_SOLID_COLOR_DARK_GRAY_BUTTON
+import com.google.jetpackcamera.ui.components.capture.TEST_PATTERN_SOLID_COLOR_GRAY_BUTTON
+import com.google.jetpackcamera.ui.components.capture.TEST_PATTERN_SOLID_COLOR_GREEN_BUTTON
+import com.google.jetpackcamera.ui.components.capture.TEST_PATTERN_SOLID_COLOR_RED_BUTTON
+import com.google.jetpackcamera.ui.components.capture.TEST_PATTERN_SOLID_COLOR_WHITE_BUTTON
 import com.google.jetpackcamera.ui.uistate.capture.DebugUiState
 import kotlin.math.abs
 
@@ -183,6 +197,7 @@ private fun MainDebugOverlay(
         }
 
         TextButton(
+            modifier = Modifier.testTag(DEBUG_OVERLAY_SET_TEST_PATTERN_BUTTON),
             enabled = debugUiState.availableTestPatterns.size > 1,
             onClick = {
                 onMoveToComponent(SelectedDialog.SetTestPattern)
@@ -271,7 +286,8 @@ private fun SetTestPatternDialog(
         LazyColumn {
             items(sortedTestPatterns) { testPattern ->
                 FilledIconToggleButton(
-                    modifier = Modifier.fillMaxWidth(),
+                    modifier = Modifier.fillMaxWidth()
+                        .testTag(getTestPatternTag(testPattern)),
                     checked = testPattern == selectedTestPattern,
                     onCheckedChange = { selected ->
                         if (selected) {
@@ -288,10 +304,31 @@ private fun SetTestPatternDialog(
             horizontalAlignment = Alignment.End
         ) {
             TextButton(
+                modifier = Modifier.testTag(TEST_PATTERN_DIALOG_CLOSE_BUTTON),
                 onClick = { onClose() }
             ) {
                 Text(text = "Close")
             }
+        }
+    }
+}
+
+private fun getTestPatternTag(testPattern: TestPattern): String {
+    return when (testPattern) {
+        TestPattern.Off -> TEST_PATTERN_OFF_BUTTON
+        TestPattern.ColorBars -> TEST_PATTERN_COLOR_BARS_BUTTON
+        TestPattern.ColorBarsFadeToGray -> TEST_PATTERN_COLOR_BARS_FADE_TO_GRAY_BUTTON
+        TestPattern.PN9 -> TEST_PATTERN_PN9_BUTTON
+        TestPattern.Custom1 -> TEST_PATTERN_CUSTOM1_BUTTON
+        is TestPattern.SolidColor -> when (testPattern) {
+            TestPattern.SolidColor.RED -> TEST_PATTERN_SOLID_COLOR_RED_BUTTON
+            TestPattern.SolidColor.GREEN -> TEST_PATTERN_SOLID_COLOR_GREEN_BUTTON
+            TestPattern.SolidColor.BLUE -> TEST_PATTERN_SOLID_COLOR_BLUE_BUTTON
+            TestPattern.SolidColor.WHITE -> TEST_PATTERN_SOLID_COLOR_WHITE_BUTTON
+            TestPattern.SolidColor.BLACK -> TEST_PATTERN_SOLID_COLOR_BLACK_BUTTON
+            TestPattern.SolidColor.GRAY -> TEST_PATTERN_SOLID_COLOR_GRAY_BUTTON
+            TestPattern.SolidColor.DARK_GRAY -> TEST_PATTERN_SOLID_COLOR_DARK_GRAY_BUTTON
+            else -> ""
         }
     }
 }
