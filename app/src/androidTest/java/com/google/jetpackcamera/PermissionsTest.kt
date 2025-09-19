2be9eadcb8c0f7fcf19e99f541d574fb9ef8d13a
Trevor McGuire <trevormcguire@google.com>
2025-09-19 13:08:45 -0700

    Replace internally defined permission constant with SDK constant

--- app/src/androidTest/java/com/google/jetpackcamera/PermissionsTest.kt
+++ app/src/androidTest/java/com/google/jetpackcamera/PermissionsTest.kt
@@ -15,6 +15,8 @@
  */
 package com.google.jetpackcamera
 
+import android.Manifest.permission.CAMERA
+import android.Manifest.permission.RECORD_AUDIO
 import androidx.compose.ui.test.isDisplayed
 import androidx.compose.ui.test.isNotDisplayed
 import androidx.compose.ui.test.junit4.createEmptyComposeRule
@@ -50,8 +52,6 @@ import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
-const val CAMERA_PERMISSION = "android.permission.CAMERA"
-
 @RunWith(AndroidJUnit4::class)
 class PermissionsTest {
     @get:Rule
@@ -65,7 +65,7 @@ class PermissionsTest {
 
     @get:Rule
     val cameraAudioPermissionRule = IndividualTestGrantPermissionRule(
-        permissions = arrayOf(CAMERA_PERMISSION, android.Manifest.permission.RECORD_AUDIO),
+        permissions = arrayOf(CAMERA, RECORD_AUDIO),
         targetTestNames = arrayOf(
             "writeStoragePermission_granted",
             "writeStoragePermission_denied"
@@ -74,7 +74,7 @@ class PermissionsTest {
 
     @get:Rule
     val cameraPermissionRule = IndividualTestGrantPermissionRule(
-        permissions = arrayOf(CAMERA_PERMISSION),
+        permissions = arrayOf(CAMERA),
         targetTestNames = arrayOf(
             "recordAudioPermission_granted_closesPage",
             "recordAudioPermission_denied_closesPage"
