cf5c60cbebbc02038029a63c6051f984a2e6b1df
davidjiagoogle <davidjia@google.com>
2026-05-06 01:22:04 +0000

    Format CameraModule using Spotless to satisfy JCA Kotlin rules

--- data/camera/src/main/java/com/google/jetpackcamera/data/camera/CameraModule.kt
+++ data/camera/src/main/java/com/google/jetpackcamera/data/camera/CameraModule.kt
@@ -17,7 +17,6 @@ package com.google.jetpackcamera.data.camera
 
 import android.app.Application
 import android.content.Context
-import dagger.hilt.android.qualifiers.ApplicationContext
 import com.google.jetpackcamera.core.camera.CameraXCameraSystem
 import com.google.jetpackcamera.core.camera.lowlight.LowLightBoostAvailabilityChecker
 import com.google.jetpackcamera.core.camera.lowlight.LowLightBoostEffectProvider
@@ -32,6 +31,7 @@ import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Provider
 import javax.inject.Singleton
