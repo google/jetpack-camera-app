775b2fcdf838dc68ec53042af8104336bddd970d
Trevor McGuire <trevormcguire@google.com>
2026-07-03 06:33:35 +0000

    Upgrade hilt dependency and use new hiltViewModel package

--- feature/preview/src/main/java/com/google/jetpackcamera/feature/preview/PreviewScreen.kt
+++ feature/preview/src/main/java/com/google/jetpackcamera/feature/preview/PreviewScreen.kt
@@ -56,7 +56,7 @@ import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.lifecycle.compose.LifecycleStartEffect
 import androidx.tracing.Trace
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
