0a4a7f80a5b6bb00710a431e9282cd808c7934a2
Trevor McGuire <trevormcguire@google.com>
2026-08-10 20:49:28 +0000

    Revert moving overlap tests to androidTest; rely on Robolectric and ui-test-manifest instead

--- ui/components/capture/src/test/java/com/google/jetpackcamera/ui/components/capture/OverlapAwareStyleProviderTest.kt
+++ ui/components/capture/src/test/java/com/google/jetpackcamera/ui/components/capture/OverlapAwareStyleProviderTest.kt
@@ -25,13 +25,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.unit.dp
-import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.google.common.truth.Truth.assertThat
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
 
-@RunWith(AndroidJUnit4::class)
+@RunWith(RobolectricTestRunner::class)
 class OverlapAwareStyleProviderTest {
 
     @get:Rule
